cf5303820f235108714e851b56a06076cd63313e
Abhigyan Khaund <20434085+abhigyank@users.noreply.github.com>
2018-09-08 10:01:49 +0530

    sys arg: Take dataset number as argument in command line as well.

--- Assignment1/code.py
+++ Assignment1/code.py
@@ -4,9 +4,15 @@ import LinearlySeperableWithDiagonalMatrix
 import LinearlySeperableWithNonDiagonalMatrix
 import statistics_func as sf	
 import matplotlib.pyplot as plt
-Class1_train,Class1_test=sf.get_data("Data2/Class1.txt")
-Class2_train,Class2_test=sf.get_data("Data2/Class2.txt")
-Class3_train,Class3_test=sf.get_data("Data2/Class3.txt")
+
+if sys.argv[2]=='1':
+	Class1_train,Class1_test=sf.get_data("Data1/Class1.txt")
+	Class2_train,Class2_test=sf.get_data("Data1/Class2.txt")
+	Class3_train,Class3_test=sf.get_data("Data1/Class3.txt")
+if sys.argv[2]=='2':
+	Class1_train,Class1_test=sf.get_data("Data2/Class1.txt")
+	Class2_train,Class2_test=sf.get_data("Data2/Class2.txt")
+	Class3_train,Class3_test=sf.get_data("Data2/Class3.txt")
 sf.plot(Class1_train,'mo')
 sf.plot(Class2_train,'yo')
 sf.plot(Class3_train,'co')
