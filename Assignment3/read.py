ddc111027890a2a4091165b97ca8f33897ffd999
Abhigyan Khaund <20434085+abhigyank@users.noreply.github.com>
2018-10-28 12:19:10 +0530

    Assignment 3: Data and read files.

---
+++ Assignment3/read.py
@@ -0,0 +1,11 @@
+import numpy as np
+def get_numpy_from_file(filePath):
+	np_array = []
+	with open(filePath, "r") as f:
+		for line in f:
+			arr = map(float,line.split())
+			np_array.append(arr)
+		
+	np_array = np.array(np_array)
+	return np_array
+ 
